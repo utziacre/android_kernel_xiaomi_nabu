0c9f5cbf2e6d7235f829ef3ae6254a81bb90d204
Luis Claudio R. Goncalves <lgoncalv@redhat.com>
2021-05-24 16:58:26 -0300

    printk: revamp "Make rt aware"
    Commit 7fcfb8fdfc41e ("printk: Make rt aware") from v4.19-rt is more
    comprehensive than its equivalent in v4.14-rt (commit 70ce7ac4c16f1).
    
    As one example, this commit resolves the following build warnings from
    v4.14-rt:
    
      kernel/printk/printk.c: At top level:
      kernel/printk/printk.c:1608:13: warning: ‘console_lock_spinning_enable’ defined but not used [-Wunused-function]
       static void console_lock_spinning_enable(void)
                   ^
      kernel/printk/printk.c:1633:12: warning: ‘console_lock_spinning_disable_and_check’ defined but not used [-Wunused-function]
       static int console_lock_spinning_disable_and_check(void)
                  ^
    
    Note: fixed a migrate_enable/disable imbalance in printk_late_init() in
          v4.14.232-rt113.
    
    Fixes: 70ce7ac4c16f1 ("printk: Make rt aware")
    Fixes: 10de28bbf31a1 (printk: revamp "Make rt aware")
    Signed-off-by: Luis Claudio R. Goncalves <lgoncalv@redhat.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -1585,6 +1585,7 @@ SYSCALL_DEFINE3(syslog, int, type, char __user *, buf, int, len)
 	return do_syslog(type, buf, len, SYSLOG_FROM_READER);
 }
 
+#ifndef CONFIG_PREEMPT_RT_FULL
 /*
  * Special console_lock variants that help to reduce the risk of soft-lockups.
  * They allow to pass console_lock to another printk() call using a busy wait.
@@ -1724,6 +1725,15 @@ static int console_trylock_spinning(void)
 
 	return 1;
 }
+
+#else
+
+static int console_trylock_spinning(void)
+{
+	return console_trylock();
+}
+
+#endif
 
 /*
  * Call the console drivers, asking them to write out
@@ -2864,6 +2874,7 @@ static int __init printk_late_init(void)
 	struct console *con;
 	int ret = 0;
 
+	migrate_disable();
 	for_each_console(con) {
 		if (!(con->flags & CON_BOOT))
 			continue;
@@ -2892,6 +2903,7 @@ static int __init printk_late_init(void)
 					console_cpu_notify, NULL);
 	WARN_ON(ret < 0);
 #endif
+	migrate_enable();
 	return ret;
 }
 late_initcall(printk_late_init);
