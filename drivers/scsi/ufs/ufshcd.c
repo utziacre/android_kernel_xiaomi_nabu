f27be0f49ed1e7f77caf016f35c424fcc0c95479
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-07-06 13:56:49 +0200

    drivers: scsi: Import Xiaomi changes
    Change-Id: I0b560cec06b2263054000e7fdda37934c4d07cca

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -213,6 +213,10 @@ static void ufshcd_update_uic_error_cnt(struct ufs_hba *hba, u32 reg, int type)
 /* UIC command timeout, unit: ms */
 #define UIC_CMD_TIMEOUT	500
 
+#ifdef CONFIG_MACH_XIAOMI_NABU
+#define UIC_PWR_CTRL_TIMEOUT 3000
+#endif
+
 /* NOP OUT retries waiting for NOP IN response */
 #define NOP_OUT_RETRIES    10
 /* Timeout after 30 msecs if NOP OUT hangs without response */
@@ -221,7 +225,11 @@ static void ufshcd_update_uic_error_cnt(struct ufs_hba *hba, u32 reg, int type)
 /* Query request retries */
 #define QUERY_REQ_RETRIES 3
 /* Query request timeout */
+#ifdef CONFIG_MACH_XIAOMI_NABU
+#define QUERY_REQ_TIMEOUT 3000 /* 3.0 seconds */
+#else
 #define QUERY_REQ_TIMEOUT 1500 /* 1.5 seconds */
+#endif
 
 /* Task management command timeout */
 #define TM_CMD_TIMEOUT	100 /* msecs */
@@ -426,6 +434,10 @@ static struct ufs_dev_fix ufs_fixups[] = {
 		UFS_DEVICE_QUIRK_HOST_PA_SAVECONFIGTIME),
 	UFS_FIX(UFS_VENDOR_SKHYNIX, UFS_ANY_MODEL,
 		UFS_DEVICE_QUIRK_WAIT_AFTER_REF_CLK_UNGATE),
+#ifdef CONFIG_MACH_XIAOMI_NABU
+	UFS_FIX(UFS_VENDOR_SANDISK, UFS_ANY_MODEL,
+		UFS_DEVICE_QUIRK_WAIT_AFTER_REF_CLK_UNGATE),
+#endif
 	UFS_FIX(UFS_VENDOR_SKHYNIX, "hB8aL1",
 		UFS_DEVICE_QUIRK_HS_G1_TO_HS_G3_SWITCH),
 	UFS_FIX(UFS_VENDOR_SKHYNIX, "hC8aL1",
@@ -5115,7 +5127,11 @@ static int ufshcd_uic_pwr_ctrl(struct ufs_hba *hba, struct uic_command *cmd)
 
 more_wait:
 	if (!wait_for_completion_timeout(hba->uic_async_done,
+#ifdef CONFIG_MACH_XIAOMI_NABU
+					 msecs_to_jiffies(UIC_PWR_CTRL_TIMEOUT))) {
+#else
 					 msecs_to_jiffies(UIC_CMD_TIMEOUT))) {
+#endif
 		u32 intr_status = 0;
 		s64 ts_since_last_intr;
 
@@ -5626,7 +5642,11 @@ static int ufshcd_complete_dev_init(struct ufs_hba *hba)
 	 * Some vendor devices are taking longer time to complete its internal
 	 * initialization, so set fDeviceInit flag poll time to 5 secs
 	 */
+#ifdef CONFIG_MACH_XIAOMI_NABU
+	timeout = ktime_add_ms(ktime_get(), 8000);
+#else
 	timeout = ktime_add_ms(ktime_get(), 5000);
+#endif
 
 	/* poll for max. 5sec for fDeviceInit flag to clear */
 	while (1) {
