43bf2c21a508a87fece1e3fbd9f7111face84a2c
ztc1997 <dev@ztc1997.com>
2024-04-08 21:05:32 +0800

    crypto: zstd: Change `compression_level` type to int
    Adjusted the data type for `compression_level` to int to support negative values as valid compression levels in zstd.

--- crypto/zstd.c
+++ crypto/zstd.c
@@ -23,7 +23,7 @@
 #include <crypto/internal/scompress.h>
 
 
-static uint __read_mostly compression_level = 1;
+static int __read_mostly compression_level = 1;
 
 int set_compression_level(const char *val, const struct kernel_param *kp)
 {
@@ -38,6 +38,8 @@ int set_compression_level(const char *val, const struct kernel_param *kp)
 		temp = 1;
 	} else if (temp > zstd_max_clevel()) {
 		temp = zstd_max_clevel();
+	} else if (temp < zstd_min_clevel()) {
+		temp = zstd_min_clevel();
 	}
 
 	*((int *)kp->arg) = temp;
