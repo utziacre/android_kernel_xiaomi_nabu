d5c316610478b3a85d823eff919e7f1b0870750d
utziacre <utziacre@gmail.com>
2024-07-23 23:28:45 +0800

    Revert "input/ts: nt36xxx: Optimize interrupt CPU usage"
    This reverts commit ad210c46abcd34cdc17fd3749d371d8f8f98ba22.

--- drivers/input/touchscreen/nt36523/nt36xxx.h
+++ drivers/input/touchscreen/nt36523/nt36xxx.h
@@ -22,9 +22,7 @@
 #include <linux/delay.h>
 #include <linux/input.h>
 #include <linux/of.h>
-#include <linux/pm_qos.h>
 #include <linux/spi/spi.h>
-#include <linux/spi/spi-geni-qcom.h>
 #include <linux/uaccess.h>
 
 #ifdef CONFIG_HAS_EARLYSUSPEND
@@ -188,8 +186,6 @@ struct nvt_ts_data {
 	uint8_t *xbuf;
 	struct mutex xbuf_lock;
 	bool irq_enabled;
-	struct pm_qos_request pm_spi_req;
-	struct pm_qos_request pm_touch_req;
 	uint8_t cascade;
 	bool pen_support;
 	bool wgp_stylus;
