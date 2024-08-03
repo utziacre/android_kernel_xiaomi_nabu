787b6e0d93e5607347081e99b610709997497eb0
utziacre <utziacre@gmail.com>
2024-08-03 17:56:03 +0800

    Revert "BACKPORT: mm: multi-gen LRU: move lru_gen_add_mm() out of IRQ-off region"
    This reverts commit 5d083f9d769368ac21298fb3ee1796f7dbe23acc.

--- fs/exec.c
+++ fs/exec.c
@@ -1039,11 +1039,11 @@ static int exec_mmap(struct mm_struct *mm)
 	preempt_disable_rt();
 	active_mm = tsk->active_mm;
 	tsk->mm = mm;
+	lru_gen_add_mm(mm);
 	tsk->active_mm = mm;
 	activate_mm(active_mm, mm);
 	tsk->mm->vmacache_seqnum = 0;
 	vmacache_flush(tsk);
-	lru_gen_add_mm(mm);
 	preempt_enable_rt();
 	task_unlock(tsk);
 	lru_gen_use_mm(mm);
